d0394391f435698d33ddd91a2fc26022b77de600
FORGE096 <forge1803@gmail.com>
2025-12-29 17:37:33 +0330

    Update pubspec.yaml to include additional dependencies and improve project structure

---
+++ server/lib/ayumc_server.dart
@@ -0,0 +1,13 @@
+class AyuMCServer {
+  static Future<void> start() async {
+    print('Starting AyuMC Server...');
+  }
+
+  static Future<void> stop() async {
+    print('Stopping AyuMC Server...');
+  }
+
+  static Future<void> restart() async {
+    print('Restarting AyuMC Server...');
+  }
+}
